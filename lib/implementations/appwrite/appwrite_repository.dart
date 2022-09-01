08092d2a1d43c6b1d6fc51a8bcffbd9b70701dc8
felixsommerer <sommeref@dhbw-loerrach.de>
2022-09-01 03:21:46 +0200

    Created Appwrite Repository

---
+++ lib/implementations/appwrite/appwrite_repository.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_chat/chat/bloc/models/user_changed_event.dart';
+
+import 'package:cloud_chat/chat/bloc/models/initial_chat_room_state.dart';
+
+import 'package:cloud_chat/chat/bloc/models/chat_user.dart';
+
+import 'package:cloud_chat/chat/bloc/models/chat_room_metadata.dart';
+
+import 'package:cloud_chat/chat/bloc/models/chat_message.dart';
+
+import 'package:cloud_chat/bloc/models/authentification_result.dart';
+
+import '../../bloc/authentification_repository.dart';
+import '../../chat/bloc/chat_repository.dart';
+
+class AppwriteRepository implements ChatRepository, AuthenticationRepository {
+  @override
+  Future<void> createMessage(String chatRoomId, ChatMessage message) {
+    // TODO: implement createMessage
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<void> createSignOutStream() {
+    // TODO: implement createSignOutStream
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ChatUser>> getAllUsers() {
+    // TODO: implement getAllUsers
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<InitialChatRoomState> getChatRoom(String chatRoomId) {
+    // TODO: implement getChatRoom
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<InitialChatRoomState> getChatRoomStream(String chatRoomId) {
+    // TODO: implement getChatRoomStream
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<UserChangedEvent> getUserStream() {
+    // TODO: implement getUserStream
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isCurrentProfileCompleted() {
+    // TODO: implement isCurrentProfileCompleted
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setFullName(String fullName) {
+    // TODO: implement setFullName
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthentificationResult> signInWithUsernameAndPasswordAsync(
+      String username, String password) {
+    // TODO: implement signInWithUsernameAndPasswordAsync
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthentificationResult> signUpWithUsernameAndPassword(
+      String username, String password) {
+    // TODO: implement signUpWithUsernameAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateChatRoom(ChatRoomMetadata metadata) {
+    // TODO: implement updateChatRoom
+    throw UnimplementedError();
+  }
+}
